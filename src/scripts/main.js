4a44bd209d788391e62767643002118bc22f3c31
Ivan <drofyakivan2008@gmail.com>
2026-02-15 22:53:19 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery__list');
+const mainImage = document.querySelector('img[alt="main image"]');
+
+gallery.addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  e.preventDefault();
+
+  mainImage.src = link.href;
+});
