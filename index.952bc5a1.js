72015a43715eb0732e4764b306ae72b903d6fff4
Ivan <drofyakivan2008@gmail.com>
2026-02-16 12:29:46 +0200

    make build

---
+++ index.952bc5a1.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.getElementById("largeImg");e&&e.addEventListener("click",function(e){var l=e.target.closest(".list-item__link");l&&t&&(e.preventDefault(),t.src=l.href)});
+//# sourceMappingURL=index.952bc5a1.js.map
